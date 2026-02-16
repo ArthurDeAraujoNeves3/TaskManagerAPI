beceb2e77d72a4db7b5cd4776c3c7d04edcee781
ArthurDeAraujoNeves3 <arthurdearaujoneves333@gmail.com>
2026-02-16 19:25:25 -0300

    feat: Estrutura de pastas do módulo de usuários

---
+++ src/main/java/com/arthurneves/TaskManager/modules/users/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.arthurneves.TaskManager.modules.users.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+
+    @GetMapping("/create")
+    public String create() {
+        return "Olá";
+    }
+}
