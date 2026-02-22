53464b1857f4d3c167ee168ac112e8adeb7f469b
ArthurDeAraujoNeves3 <arthurdearaujoneves333@gmail.com>
2026-02-22 19:57:27 -0300

    chore: Criado controller de autenticação

--- src/main/java/com/arthurneves/TaskManager/modules/users/controllers/UserController.java
+++ src/main/java/com/arthurneves/TaskManager/modules/users/controllers/UserController.java
@@ -1,41 +1,5 @@
 package com.arthurneves.TaskManager.modules.users.controllers;
 
-import com.arthurneves.TaskManager.modules.users.dto.UserLoginRequestDTO;
-import com.arthurneves.TaskManager.modules.users.entity.UserEntity;
-import com.arthurneves.TaskManager.modules.users.useCases.CreateUserUseCase;
-import com.arthurneves.TaskManager.modules.users.useCases.LoginUseCase;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RequestMapping("/auth")
-@RestController
 public class UserController {
 
-    @Autowired
-    private CreateUserUseCase createUserUseCase;
-
-    @Autowired
-    private LoginUseCase loginUseCase;
-
-    @PostMapping("/register")
-    public ResponseEntity<Object> register(@Valid @RequestBody UserEntity body) {
-        try {
-            UserEntity user = createUserUseCase.execute(body);
-            return ResponseEntity.ok().body(user);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<Object> login(@Valid @RequestBody UserLoginRequestDTO body) {
-        try {
-            Object result = loginUseCase.execute(body);
-            return ResponseEntity.ok().body(result);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
 }
