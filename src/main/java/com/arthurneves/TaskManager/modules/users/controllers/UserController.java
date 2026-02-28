d04acca87650cebb1c6bcc45a87bc6df908f1d36
ArthurDeAraujoNeves3 <arthurdearaujoneves333@gmail.com>
2026-02-28 17:43:23 -0300

    feat: Rota para atualizar senha

--- src/main/java/com/arthurneves/TaskManager/modules/users/controllers/UserController.java
+++ src/main/java/com/arthurneves/TaskManager/modules/users/controllers/UserController.java
@@ -1,10 +1,12 @@
 package com.arthurneves.TaskManager.modules.users.controllers;
 
 import com.arthurneves.TaskManager.modules.users.dto.UserGetInfoRequestDTO;
+import com.arthurneves.TaskManager.modules.users.dto.UserResetPasswordRequestDTO;
 import com.arthurneves.TaskManager.modules.users.dto.UserUpdateRequestDTO;
-import com.arthurneves.TaskManager.modules.users.entity.UserEntity;
 import com.arthurneves.TaskManager.modules.users.useCases.GetUserInfoUseCase;
+import com.arthurneves.TaskManager.modules.users.useCases.ResetPasswordUseCase;
 import com.arthurneves.TaskManager.modules.users.useCases.UpdateUserDataUseCase;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +16,10 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Autowired
     private GetUserInfoUseCase getUserInfoUseCase;
-
     @Autowired
     private UpdateUserDataUseCase updateUserDataUseCase;
+    @Autowired
+    private ResetPasswordUseCase resetPasswordUseCase;
 
     @GetMapping("/info")
     public ResponseEntity<Object> returnUserInfo(@RequestHeader("Authorization") String token) {
@@ -33,6 +36,16 @@ public class UserController {
         try {
             UserUpdateRequestDTO result = updateUserDataUseCase.execute(token, data);
             return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/resetpassword")
+    public ResponseEntity<Object> resetUserPassword(@RequestHeader("Authorization") String token, @Valid @RequestBody UserResetPasswordRequestDTO data) {
+        try {
+            this.resetPasswordUseCase.execute(token, data);
+            return ResponseEntity.ok().body("Senha atualizada com sucesso!");
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
---
+++ src/main/java/com/arthurneves/TaskManager/modules/users/useCases/ResetPasswordUseCase.java
@@ -0,0 +1,50 @@
+package com.arthurneves.TaskManager.modules.users.useCases;
+
+import com.arthurneves.TaskManager.exceptions.UserNotFound;
+import com.arthurneves.TaskManager.exceptions.WrongPasswordException;
+import com.arthurneves.TaskManager.modules.users.dto.UserResetPasswordRequestDTO;
+import com.arthurneves.TaskManager.modules.users.entity.UserEntity;
+import com.arthurneves.TaskManager.modules.users.repositories.UserRepository;
+import com.arthurneves.TaskManager.providers.JWTProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ResetPasswordUseCase {
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private JWTProvider jwtProvider;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void execute(String token, UserResetPasswordRequestDTO data) {
+        UUID id = UUID.fromString(this.jwtProvider.validateToken(token));
+
+        Optional<UserEntity> user = this.repository.findById(id);
+
+        if ( user.isEmpty() ) {
+            throw new UserNotFound();
+        }
+
+        UserEntity userEntity = user.get();
+        boolean passwordMatches = passwordEncoder.matches(data.getPassword(), userEntity.getPassword());
+        if ( !passwordMatches ) {
+            throw new WrongPasswordException();
+        }
+
+        UserEntity updatedUser = UserEntity.builder()
+                .id(id)
+                .username(userEntity.getUsername())
+                .name(userEntity.getName())
+                .email(userEntity.getEmail())
+                .password(data.getNewPassword())
+                .build();
+
+        this.repository.save(updatedUser);
+    }
+}
